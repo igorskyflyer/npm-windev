c3cd90b1a8956611e0c9d4341917195cb7ffc8a8
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-29 02:07:45 +0200

    Add main entry point

---
+++ main.js
@@ -0,0 +1,24 @@
+/**
+ * Checks whether the given directory
+ * or file name is a legacy Windows device.
+ * @param {string} name
+ * @returns {boolean}
+ */
+function isWindowsDevice(name) {
+  if (typeof name !== 'string') {
+    return false
+  }
+
+  // find out more here,
+  // https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file?redirectedfrom=MSDN#Naming_Conventions
+  const winDevicesMatcher = new RegExp(
+    '^\\s*(?:CON|PRN|AUX|NUL|COM1|COM2|COM3|COM4|COM5|COM6|COM7|COM8|COM9|LPT1|LPT2|LPT3|LPT4|LPT5|LPT6|LPT7|LPT8|LPT9)\\s*(?:\\.{1}[^\\.]{0,}){0,}\\s*$',
+    'i'
+  )
+
+  return winDevicesMatcher.test(name)
+}
+
+module.exports = {
+  isWindowsDevice,
+}
