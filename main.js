f7dc9e0544a5f325ec27070624279ce243a4787b
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-29 02:11:23 +0200

    Update quantifiers

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function isWindowsDevice(name) {
   // find out more here,
   // https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file?redirectedfrom=MSDN#Naming_Conventions
   const winDevicesMatcher = new RegExp(
-    '^\\s*(?:CON|PRN|AUX|NUL|COM1|COM2|COM3|COM4|COM5|COM6|COM7|COM8|COM9|LPT1|LPT2|LPT3|LPT4|LPT5|LPT6|LPT7|LPT8|LPT9)\\s*(?:\\.{1}[^\\.]{0,}){0,}\\s*$',
+    '^\\s*(?:CON|PRN|AUX|NUL|COM1|COM2|COM3|COM4|COM5|COM6|COM7|COM8|COM9|LPT1|LPT2|LPT3|LPT4|LPT5|LPT6|LPT7|LPT8|LPT9)\\s*(?:\\.{1}[^\\.]*)*\\s*$',
     'i'
   )
 
