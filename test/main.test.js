0a1b8f4e2ebee46ded8d71db97285e705353a7ea
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-30 19:51:17 +0200

    Update imports

--- test/main.test.js
+++ test/main.test.js
@@ -1,37 +1,37 @@
-import { assert as chai } from 'chai'
-import { isWindowsDevice } from '../main.js'
+const assert = require('chai').assert
+const { isWindowsDevice } = require('../main')
 
 describe('🧪 WinDev tests 🧪', () => {
   it('#1 should return false', () => {
     // @ts-ignore
-    chai.isFalse(isWindowsDevice())
+    assert.isFalse(isWindowsDevice())
   })
 
   it('#2 should return false', () => {
-    chai.isFalse(isWindowsDevice(''))
+    assert.isFalse(isWindowsDevice(''))
   })
 
   it('#3 should return true', () => {
-    chai.isTrue(isWindowsDevice('con'))
+    assert.isTrue(isWindowsDevice('con'))
   })
 
   it('#4 should return true', () => {
-    chai.isTrue(isWindowsDevice('con.txt'))
+    assert.isTrue(isWindowsDevice('con.txt'))
   })
 
   it('#5 should return true', () => {
-    chai.isTrue(isWindowsDevice('cOm3'))
+    assert.isTrue(isWindowsDevice('cOm3'))
   })
 
   it('#6 should return true', () => {
-    chai.isTrue(isWindowsDevice('COM7       '))
+    assert.isTrue(isWindowsDevice('COM7       '))
   })
 
   it('#7 should return true', () => {
-    chai.isTrue(isWindowsDevice('    COM2        .txt.al'))
+    assert.isTrue(isWindowsDevice('    COM2        .txt.al'))
   })
 
   it('#8 should return false', () => {
-    chai.isFalse(isWindowsDevice('CONnection'))
+    assert.isFalse(isWindowsDevice('CONnection'))
   })
 })
