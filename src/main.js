cd1c2145fe1e2a6ffd316c69bb0b09b349ba5eea
igorskyflyer <igor.dvlpr@gmail.com>
2021-08-01 23:18:22 +0200

    Move to src

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ export function isWindowsDevice(name) {
   }
 
   // find out more here,
-  // https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file?redirectedfrom=MSDN#Naming_Conventions
+  // https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file#naming-conventions
   const winDevicesMatcher = new RegExp(
     '^\\s*(?:CON|PRN|AUX|NUL|COM1|COM2|COM3|COM4|COM5|COM6|COM7|COM8|COM9|LPT1|LPT2|LPT3|LPT4|LPT5|LPT6|LPT7|LPT8|LPT9)\\s*(?:\\.{1}[^\\.]*)*\\s*$',
     'i'
